26e835ba79c91b7ef07f2c6b4afe3caa768ea2d3
alisson-lema <alisson_lema26@hotmail.com>
2021-01-08 14:03:18 -0500

    Ws01 helloworld

---
+++ workshops/unit1/Ws01 helloworld/helloWorld.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+	int grade=33000;
+	//printf() displays information
+	printf("Hello world from \t \" Alisson Lema \" =) \n");
+	printf("\n The grade is%d \n", grade);
+	
+	printf("size of int ; %d \n", sizeof(int));
+	
+	return 0;
+}
---
+++ workshops/unit1/Ws01 helloworld/helloWorld.java
@@ -0,0 +1,14 @@
+// Alisson Lema Hello World
+
+/*
+some lines of coments
+can be written in this way
+thaks
+*/
+class helloWorld{
+    public static void main(String[] args){
+        System.out.printf("hellow world from alisson lema");
+    }
+}
+
+
