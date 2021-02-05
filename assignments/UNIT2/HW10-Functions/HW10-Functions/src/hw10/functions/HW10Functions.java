755f379534509d248c382c63d9b369046687856b
alisson-lema <alisson_lema26@hotmail.com>
2021-02-04 22:11:42 -0500

    HW10-Functions

--- assignments/UNIT2/HW10-Functions/HW10-Functions/src/hw10/functions/HW10Functions.java
+++ assignments/UNIT2/HW10-Functions/HW10-Functions/src/hw10/functions/HW10Functions.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package hw10.functions;
 
-/**
- *
- * @author HP
- */
+package hw10.functions;
+import java.util.Scanner;
+import java.math.*;
+
 public class HW10Functions {
 
     /**
@@ -16,55 +10,82 @@ public class HW10Functions {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int multiplying = (int) 8.5;
-        int multiplier = (int) 18.15;
-        float result = 0;
+        Scanner input = new Scanner(System.in);
+        int divd;
+        int divr;
+        float quotient = 0;
         int x = 0;
         int y = 0;
+        int multiplicand;
+        int product;
+        int top = 12;
         
-        //call to multiplyTwoNumbers () function
-        result = multiplyTwoNumbers(multiplying, multiplier);
+        System.out.println("======= ALISSON LEMA CAHVEZ =========");
         
-        System.out.println("Result of 8.5 and 18.15 is  -> " + result); //91.975 or ?
+        System.out.println("Insert a number for divd -->");
+        divd = input.nextInt();
         
-        y = f(6);
-        System.out.println("6^4 ->" + y);
+        System.out.println("Insert a number for divr -->");
+        divr = input.nextInt();
+                
         
-        y = f(-6);
-        System.out.println("-6^4 ->" + y); //625
-        
-        x = -1;
+        quotient = divideTwoNumbers(divd, divr);
+
+        System.out.println("Quotient is -> " + quotient);
+
+        System.out.println("Insert a number for fuction -->");
+        x = input.nextInt();
         y = f(x);
-        System.out.println("the cube of ->" + x + " <- is equalto -> " + y);
+        System.out.println("the square of -> " + x + " <- is equal to -> " + y);
         
-    }
-    //function definition
-    //return data type   name  parameter1  parameter2
-    public static float multiplyTwoNumbers(int multiplying, int multiplier) {
-        //body of the function
-        float result = 0.0F;
+        System.out.println("Insert a number for fuction -->");
+        x = input.nextInt();
+        y = g(x);
+        System.out.println("the value of -> " + x + " <- is equal to -> " + y);
         
-        //             6.5      14.15  -> 91.975
-        //result       int       int 
-        result =  (float)multiplying * (float)multiplier;
-        return result;
-        
-    }
-    
-    public static int f(int x){
-        int y = 0;
-        
-        y = x * x;
-        
-        return y;
-    }
-    
-    public static int g(int x){
-        int y = 0;
-        
-        y = x * x + 2 * x + 1;
-        
-        return y;
-    }
-    
+        System.out.println("insert a positive number");
+        multiplicand = input.nextInt();
+        for (int multiplier = 1 ; multiplier <= top ; multiplier++){
+            product = gettable (multiplicand, multiplier);
+            System.out.println(multiplicand  + " x " + multiplier + " = " + product);
+        }
 }
+
+    
+    public static float divideTwoNumbers(int dividend, int divisor) {
+        
+        float quotient = 0.0F;
+
+       
+        quotient = (float) dividend / (float) divisor;
+
+        return quotient;
+    }
+
+    public static int f(int x) {
+        int y = 0;
+
+        y = x * x;
+
+        return y;
+    }
+
+    public static int g(int x) {
+        int y = 0;
+
+        y = x * x + 2 * x + 1;
+
+        return y;
+    }
+    
+    public static int gettable(int multiplicand, int multiplier){
+        int product;
+        
+        product = multiplicand*multiplier;
+        
+        return product;
+    }
+
+}
+    
+
